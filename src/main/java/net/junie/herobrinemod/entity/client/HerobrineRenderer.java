418cc430ad6161947cd27981790021aebfa04eae
JunieMoe <128286028+JunieMoe@users.noreply.github.com>
2025-10-15 16:43:11 +0100

    GRADLE FIXED

--- src/main/java/net/junie/herobrinemod/entity/client/HerobrineRenderer.java
+++ src/main/java/net/junie/herobrinemod/entity/client/HerobrineRenderer.java
@@ -3,15 +3,16 @@ package net.junie.herobrinemod.entity.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.junie.herobrinemod.HerobrineMod;
 import net.junie.herobrinemod.entity.custom.HerobrineEntity;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class HerobrineRenderer extends HumanoidMobRenderer<HerobrineEntity, HerobrineModel<HerobrineEntity>> {
+public class HerobrineRenderer extends HumanoidMobRenderer<HerobrineEntity, HumanoidModel<HerobrineEntity>> {
 
     public HerobrineRenderer(EntityRendererProvider.Context pContext) {
-        super(pContext, new HerobrineModel<>(pContext.bakeLayer(MobModelLayers.HEROBRINE_LAYER)), 0.5f);
+        super(pContext, new HumanoidModel<>(pContext.bakeLayer(MobModelLayers.HEROBRINE_LAYER)), 0.5f);
     }
 
     @Override
